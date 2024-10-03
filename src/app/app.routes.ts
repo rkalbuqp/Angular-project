904ed73365fb4284d61992788aca29d3a0d00220
Luan Anderson <luan.anderson.001@gmail.com>
2024-10-03 20:40:19 -0300

    add-Pages-Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PromotionsComponent } from './Pages/promotions/promotions.component';
+import { ContactUsComponent } from './Pages/contact-us/contact-us.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { AboutComponent } from './Pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'promotions', component: PromotionsComponent },
+  { path: 'contact', component: ContactUsComponent },
+  { path: 'about', component: AboutComponent },
+];
