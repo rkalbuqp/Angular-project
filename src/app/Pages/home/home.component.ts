f5acd6d89deaadd3dcdf7bfbdd74754e03c53f0f
Cost_Victor <victorcost436@gmail.com>
2024-10-06 20:25:00 -0300

    feat: componente container grid da home

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CardBuyComponent } from '@components/card-buy/card-buy.component';
+import { ContainerGridComponent } from '@components/container-grid/container-grid.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CardBuyComponent],
+  imports: [CardBuyComponent, ContainerGridComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
