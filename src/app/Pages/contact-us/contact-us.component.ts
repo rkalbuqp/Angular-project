efce6eaec16a307846a38f2e6efde891c5683732
Kassia Albuquerque <kassiademaria@gmail.com>
2024-10-07 22:46:23 -0300

    Corrigindo layout da página de contatação e organizando algumas coisas no component shared footer.

--- src/app/Pages/contact-us/contact-us.component.ts
+++ src/app/Pages/contact-us/contact-us.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ContainerGridComponent } from '../../../shared/components/container-grid/container-grid.component';
+import { ImageBlockComponent } from '../../../shared/components/image-block/image-block.component';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [],
+  imports: [ContainerGridComponent, ImageBlockComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss',
 })
-export class ContactUsComponent { }
+export class ContactUsComponent {}
